64999a503ebb2e756a41fea95b6558fba4fd22a8
dugq <1184116431@qq.com>
2018-01-12 11:45:56 +0800

    启动参数

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,23 +1,19 @@
 package com.example;
 
-import com.example.BasicComponent.BasicComponent;
 import com.example.intercepter.ValidatorInterception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import javax.sql.DataSource;
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -30,23 +26,18 @@ public class DemoApplication extends WebMvcConfigurerAdapter {
 //				.allowedMethods("GET", "POST", "DELETE", "PUT","OPTIONS").maxAge(3600);
 //	}
 
-    /**
-     * 自己选择事务实现
-     * @param dataSource
-     * @return
-     */
-    @Bean("txManage")
-	public PlatformTransactionManager txManager(DataSource dataSource){
-		return new DataSourceTransactionManager(dataSource);
-	}
-
-	@Bean
-	public Object testTx(PlatformTransactionManager platformTransactionManager){
-        logger.info("========TX_MANAGER==========================="+platformTransactionManager.getClass().getName());
-		return null;
-	}
-
 	public static void main(String[] args) {
+		//启动参数添加：--Dfile.encoding=UTF-8 --mysql.database=spring-boot --server.port=8090 --mycofig.database=spring_boot 跟yml同名的属性将覆盖
+		ApplicationArguments applicationArguments = new DefaultApplicationArguments(
+				args);
+		logger.info("启动参数："+Arrays.toString(args));
+		//获取启动参数
+//		List<String> optionValues = applicationArguments.getOptionValues("Dmysql.database");
+		//获取参数名称
+//		Set<String> optionNames = applicationArguments.getOptionNames();
+		//获取所有的参数即：args
+//		String[] sourceArgs = applicationArguments.getSourceArgs();
+
 		SpringApplication.run(DemoApplication.class, args);
 		logger.error("CONGRATULATIONS!!   demo effective!");
     }
